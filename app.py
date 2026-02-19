affad89b5bf47304afefeb2827b8cdd66684e2db
Tirumalesh44 <tiru5210@gmail.com>
2026-02-19 22:04:02 +0530

    second  commit model change added router

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = FastAPI(title="Parental Control Backend")
 Base.metadata.create_all(bind=engine)
 
 HF_TOKEN = os.getenv("HF_TOKEN")
-HF_API_URL = "https://api-inference.huggingface.co/models/Falconsai/nsfw_image_detection"
+HF_API_URL = "https://router.huggingface.co/hf-inference/models/Falconsai/nsfw_image_detection"
 
 SEXUAL_THRESHOLD = 0.6
 
